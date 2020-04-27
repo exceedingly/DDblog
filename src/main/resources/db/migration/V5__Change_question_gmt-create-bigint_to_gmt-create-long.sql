99fa356c9630278040a63f21ddeb94fb332353ca
mcd <1323086220@qq.com>
2020-04-28 02:54:50 +0800

    数据库显示发表时间 本地不显示 数据库发表汉字文章失效 ;

---
+++ src/main/resources/db/migration/V5__Change_question_gmt-create-bigint_to_gmt-create-long.sql
@@ -0,0 +1,2 @@
+ALTER TABLE question MODIFY gmt_create long;
+ALTER TABLE question MODIFY gmt_modified long;
---
+++ src/main/resources/db/migration/V6__Change_question_gmt-create-long_to_gmt-create-bigint.sql
@@ -0,0 +1,2 @@
+ALTER TABLE question MODIFY gmt_create BIGINT;
+ALTER TABLE question MODIFY gmt_modified BIGINT;
